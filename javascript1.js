0fab4b3d7e81eb3e4eea73628fc079e948523f8a
AijazAli777 <aijazalee889@gmail.com>
2023-08-25 15:25:09 +0500

    Swap without using temp variable.

--- javascript1.js
+++ javascript1.js
@@ -2,13 +2,23 @@ var valueOne = 1;
 var valueTwo = 2;
 var tempValue;
 
-swapByUsingTempVariable();
+//swapByUsingTempVariable();
+swapWithoutUsingTempVariable()
 
 function swapByUsingTempVariable()
 {
     tempValue = valueOne;
     valueOne = valueTwo;
     valueTwo = tempValue;
+
+    console.log("valueOne = "+valueOne+"\nvalueTwo = "+valueTwo);
+}
+
+function swapWithoutUsingTempVariable()
+{
+    valueOne = valueOne + valueTwo;
+    valueTwo = valueOne - valueTwo;
+    valueOne = valueOne - valueTwo;
 
     console.log("valueOne = "+valueOne+"\nvalueTwo = "+valueTwo);
 }
