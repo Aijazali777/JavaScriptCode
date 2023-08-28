45861c341eb527da412bdd2ae72a0f8241209aca
AijazAli777 <aijazalee889@gmail.com>
2023-08-28 15:16:09 +0500

    Implemented at() method

--- arrayMethods_1.js
+++ arrayMethods_1.js
@@ -4,9 +4,17 @@
 var myArray = [1, 2, 3, 4];
 var sumOfArr = 0;
 myArray.forEach(sumOfArray);
-console.log("Sum: "+sumOfArr);
+console.log("Sum Of Array = "+sumOfArr);
 
 function sumOfArray(item)
 {
     sumOfArr = sumOfArr +item;
 }
+
+// The at() method returns an indexed element from an array.
+
+const myFruits = ['Apple', 'Banana', 'Orange', 'Grape'];
+let index = 2;
+
+let fruit = myFruits.at(index);
+console.log("Fruit available at index ("+index+") is "+fruit);
