b07072b4ad69341949a6b5fa8460097de5a15ebb
AijazAli777 <aijazalee889@gmail.com>
2023-08-28 15:37:17 +0500

    Implemented concat() method

--- arrayMethods_1.js
+++ arrayMethods_1.js
@@ -1,3 +1,5 @@
+// ###### forEach() Method #########
+
 // forEach() method calls a function for each element in an array.
 // forEach() method is not executed for empty elements.
 
@@ -10,6 +12,8 @@ function sumOfArray(item)
 {
     sumOfArr = sumOfArr +item;
 }
+
+// ###### at() Method #########
 
 // The at() method returns an indexed element from an array.
 
@@ -26,3 +30,16 @@ console.log("Fruit available at first index (default)  is "+fruit);
 // The index -1 will return last element of the array
 fruit = myFruits.at(-1);
 console.log("Fruit available at last index (-1) is "+fruit);
+
+
+// ###### concat() Method #########
+
+// The concat() method will concatenates (joins) two or more arrays
+// The concat() method will return a new array, containing the joined arrays
+// The concat() method does not change the existing arrays.
+
+const fruits = ['Apple', 'Banana', 'Orange'];
+const vegetables = ['Potato', 'Tomato'];
+const items = fruits.concat(vegetables);
+console.log(items);
+
