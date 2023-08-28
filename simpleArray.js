5276261f9abc95ab7d854c4a0f46fd09e1299461
AijazAli777 <aijazalee889@gmail.com>
2023-08-28 12:47:50 +0500

    "simpleArray file has been updated "

--- simpleArray.js
+++ simpleArray.js
@@ -1,13 +1,38 @@
 var array = [1,2,3,4];
 
-loopOverArray(array);
-
-function loopOverArray(array)
-{
-    var i,
+var i,
     arrayCount = array.length;
+
+forLoopOverArray();
+whileLoopOverArray();
+doWhileLoopOverArray();
+
+function forLoopOverArray()
+{
+    console.log("FOR Loop: ");
     for( i = 0; i < arrayCount; i ++)
     {
         console.log(array[i]);
     }
+}
+
+function whileLoopOverArray()
+{
+    console.log("\nWHILE Loop: ");
+    i = 0;
+    while(i < arrayCount)
+    {
+        console.log(array[i]);
+        i++;
+    }
+}
+
+function doWhileLoopOverArray()
+{
+    console.log("\nDO WHILE Loop: ");
+    i = 0;
+    do{
+        console.log(array[i]);
+        i++;
+    }while(i < arrayCount);
 }
